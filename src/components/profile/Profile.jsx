bfac0e031534b68ed5f2a3aeb7038fcaab0f9883
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-02-15 21:09:29 +0200

    upp task 1

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -2,30 +2,29 @@ import { Avatar, Description, ProfileDiv, ProfileText, ProfileText1, Stats, Stat
 
 export const Profile = ({ username, tag, location, avatar="https://cdn-icons-png.flaticon.com/512/1077/1077012.png", stats }) => {
     return (
-        <ProfileDiv class="profile">
-  <Description class="description">
+        <ProfileDiv>
+  <Description>
     <Avatar
       src={avatar}
       alt={username}
-      class="avatar"
-    />
-    <ProfileText class="name">{username}</ProfileText>
-    <ProfileText1 class="tag">@{tag}</ProfileText1>
-    <ProfileText1 class="location">{location}</ProfileText1>
+       />
+    <ProfileText>{username}</ProfileText>
+    <ProfileText1>@{tag}</ProfileText1>
+    <ProfileText1>{location}</ProfileText1>
   </Description>
 
-  <Stats class="stats">
+  <Stats>
     <StatsEl>
-      <Label class="label">Followers</Label>
-      <span class="quantity">{stats.followers}</span>
+      <Label>Followers</Label>
+      <span>{stats.followers}</span>
     </StatsEl>
     <StatsEl>
-      <Label class="label">Views</Label>
-      <span class="quantity">{stats.views}</span>
+      <Label>Views</Label>
+      <span>{stats.views}</span>
     </StatsEl>
     <StatsEl>
-      <Label class="label">Likes</Label>
-      <span class="quantity">{stats.likes}</span>
+      <Label>Likes</Label>
+      <span>{stats.likes}</span>
     </StatsEl>
   </Stats>
 </ProfileDiv>
