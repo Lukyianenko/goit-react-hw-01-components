94339ca70845c7daffc52130cf74cb50fa4958bf
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-02-18 21:31:03 +0200

    Update Statistics.jsx

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -3,12 +3,19 @@ import {ListOfStatistics} from '../../components/statistics/ListOfStatistics';
 import PropTypes from "prop-types";
 
 export const Statistics = ({stats, title}) => {
+    if(title) {
+        return (
+            <section className={css.statistics}>
+                <h2 className={css.titles}>{title}</h2>
+                    <ListOfStatistics items={stats} />
+            </section>
+            )
+    }
     return (
-    <section className={css.statistics}>
-    <h2 className={css.titles}>{title}</h2>
-        <ListOfStatistics items={stats} />
-    </section>
-    )
+        <section className={css.statistics}>
+            <ListOfStatistics items={stats} />
+        </section>
+        )
 }
 
 
